f893b557b50956edbc0494c5797b87726e9f6b6d
ani200208 <ani200208@proton.me>
2024-01-21 18:45:45 +0200

    updating repo

---
+++ QA/Back-End-Technologies-Basics/2.JavaScript-Basics/10.Numbers-from-M-to-N.js
@@ -0,0 +1,6 @@
+function solve(m,n){
+    for (let i = m; i >=n ; i -= 1){
+        console.log(i);
+    }
+}
+solve(100, 1)
---
+++ QA/Back-End-Technologies-Basics/2.JavaScript-Basics/9.Numbers-from-1-to-5.js
@@ -0,0 +1,9 @@
+function solve() {
+    'use strict';
+
+    for (let i = 1; i <= 5; i += 1) {
+        console.log(i);
+    }
+}
+
+solve(3,2,1,4,5);
