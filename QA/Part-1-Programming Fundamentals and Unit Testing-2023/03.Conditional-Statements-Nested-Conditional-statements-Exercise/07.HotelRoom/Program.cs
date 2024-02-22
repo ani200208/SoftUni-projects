d49eed8a60480adc0697de1d2013b7b25e1e740d
ani200208 <ani200208@proton.me>
2024-02-22 15:46:56 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/03.Conditional-Statements-Nested-Conditional-statements-Exercise/07.HotelRoom/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _07.HotelRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+                    studioPrice = 50 * nights;
+                    apartmentPrice = 65 * nights;
+                    if (nights > 14)
+                    {
+                        studioPrice *= 0.7; 
+                    }
+                    else if (nights > 7)
+                    {
+                        studioPrice *= 0.95; 
+                    }
+                    break;
+                case "June":
+                case "September":
+                    studioPrice = 75.20 * nights;
+                    apartmentPrice = 68.70 * nights;
+                    if (nights > 14)
+                    {
+                        studioPrice *= 0.8; 
+                    }
+                    break;
+                case "July":
+                case "August":
+                    studioPrice = 76 * nights;
+                    apartmentPrice = 77 * nights;
+                    break;
+            }
+
+            if (nights > 14)
+            {
+                apartmentPrice *= 0.9; 
+            }
+
+            Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+        }
+    }
+    
+}
