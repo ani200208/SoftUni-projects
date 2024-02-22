fb1304c63e8d4f8bfc76e6fbaace65a7b19ff166
ani200208 <ani200208@proton.me>
2024-02-22 15:32:45 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/03.Conditional-Statements-Nested-Conditional-statements-Exercise/02.SummerClothing/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace SummerClothing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+
+            string clothing = "";
+            string shoes = "";
+
+            if (timeOfDay == "Morning")
+            {
+                if (temperature >= 10 && temperature <= 18)
+                {
+                    clothing = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (temperature > 18 && temperature <= 24)
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else
+                {
+                    clothing = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (timeOfDay == "Afternoon")
+            {
+                if (temperature >= 10 && temperature <= 18)
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (temperature > 18 && temperature <= 24)
+                {
+                    clothing = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else
+                {
+                    clothing = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+            else // Evening
+            {
+                if (temperature >= 10 && temperature <= 18)
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (temperature > 18 && temperature <= 24)
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+
+            Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+        }
+    }
+    
+}
---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/03.Conditional-Statements-Nested-Conditional-statements-Exercise/NewHome/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace NewHome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string flowerType = Console.ReadLine();
+            int flowerCount = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            double pricePerPiece = 0;
+
+            switch (flowerType)
+            {
+                case "Roses":
+                    pricePerPiece = 5;
+                    if (flowerCount > 80)
+                        pricePerPiece *= 0.9;
+                    break;
+                case "Dahlias":
+                    pricePerPiece = 3.80;
+                    if (flowerCount > 90)
+                        pricePerPiece *= 0.85;
+                    break;
+                case "Tulips":
+                    pricePerPiece = 2.80;
+                    if (flowerCount > 80)
+                        pricePerPiece *= 0.85;
+                    break;
+                case "Narcissus":
+                    pricePerPiece = 3;
+                    if (flowerCount < 120)
+                        pricePerPiece *= 1.15;
+                    break;
+                case "Gladiolus":
+                    pricePerPiece = 2.50;
+                    if (flowerCount < 80)
+                        pricePerPiece *= 1.20;
+                    break;
+            }
+
+            double totalCost = flowerCount * pricePerPiece;
+            double sumLeft = Math.Abs(budget - totalCost);
+
+            if (totalCost <= budget)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {flowerCount} {flowerType} and {sumLeft:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {sumLeft:f2} leva more.");
+            }
+        }
+    }
+}
