38f89587b2cd54c517eba5aeb1c85f74de242dae
ani200208 <ani200208@proton.me>
2024-02-22 15:41:45 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/03.Conditional-Statements-Nested-Conditional-statements-Exercise/05.Journey/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _05.Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string holidayType = "";
+            double amountSpent = 0;
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    holidayType = "Camp";
+                    amountSpent = budget * 0.3;
+                }
+                else // winter
+                {
+                    holidayType = "Hotel";
+                    amountSpent = budget * 0.7;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    holidayType = "Camp";
+                    amountSpent = budget * 0.4;
+                }
+                else // winter
+                {
+                    holidayType = "Hotel";
+                    amountSpent = budget * 0.8;
+                }
+            }
+            else // budget > 1000
+            {
+                destination = "Europe";
+                holidayType = "Hotel";
+                amountSpent = budget * 0.9;
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{holidayType} - {amountSpent:f2}");
+        }
+    }
+    
+}
