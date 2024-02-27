685d73b2217b89900b39ec418a326731f19ac3eb
ani200208 <ani200208@proton.me>
2024-02-27 14:17:09 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/10.Graduation/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _10.Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            
+            int grade = 1;
+            double sumGrades = 0;
+            int expelledGrade = 0;
+            bool expelled = false;
+
+            
+            while (grade <= 12)
+            {
+                
+                double annualGrade = double.Parse(Console.ReadLine());
+
+                
+                if (annualGrade < 4)
+                {
+                    expelled = true;
+                    expelledGrade = grade;
+                    break;
+                }
+
+                
+                sumGrades += annualGrade;
+
+                
+                grade++;
+            }
+
+           
+            if (expelled)
+            {
+                Console.WriteLine($"{name} has been excluded at {expelledGrade} grade");
+            }
+            else
+            {
+                double averageGrade = sumGrades / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+            }
+        }
+    }
+}
