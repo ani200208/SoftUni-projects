5fe0d56d455a6df9a968dc32e6c56a98c2d4fb2c
ani200208 <ani200208@proton.me>
2024-02-27 14:12:33 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/08.GreatestNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _08.GreatestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue; 
+
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                
+                if (input == "Stop")
+                    break;
+
+                
+                int number;
+                if (!int.TryParse(input, out number))
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer or 'Stop'.");
+                    continue;
+                }
+
+                
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            
+            Console.WriteLine(maxNumber);
+        }
+    }
+    
+}
