5f6aa6221a5fc9e036292b3f645b0dc2bc30e4e4
ani200208 <ani200208@proton.me>
2024-02-27 14:05:54 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/06.SequenceofNumbers2k+1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06.SequenceofNumbers2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            int counter = 1;
+
+
+            while (counter <= n)
+            {
+                Console.WriteLine(counter);
+                counter = counter * 2 + 1;
+            }
+    }   }
+}
