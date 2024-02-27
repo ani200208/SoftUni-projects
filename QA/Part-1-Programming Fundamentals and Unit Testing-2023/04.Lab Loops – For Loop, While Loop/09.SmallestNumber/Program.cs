32478e53db952876b61d69aad80c6b8e6107d2ed
ani200208 <ani200208@proton.me>
2024-02-27 14:14:20 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/09.SmallestNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _09.SmallestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue; 
+
+           
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+               
+                if (input == "Stop")
+                    break;
+
+                
+                int number;
+                if (!int.TryParse(input, out number))
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer or 'Stop'.");
+                    continue;
+                }
+
+                
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+
+            
+            Console.WriteLine(minNumber);
+        }
+    }
+    
+}
