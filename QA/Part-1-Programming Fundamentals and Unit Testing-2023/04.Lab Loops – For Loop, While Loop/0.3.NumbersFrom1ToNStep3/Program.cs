7e88a046007c3d9daec046eafbe3ebd0bd55c506
ani200208 <ani200208@proton.me>
2024-02-27 13:58:45 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/0.3.NumbersFrom1ToNStep3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _0._3.NumbersFrom1ToNStep3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 1; i <= n; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
