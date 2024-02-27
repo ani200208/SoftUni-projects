7dfdb50ca808790963985516d6735a21cf2434b9
ani200208 <ani200208@proton.me>
2024-02-27 13:51:37 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/NumbersFrom1ToN/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace 	NumbersFrom1ToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
