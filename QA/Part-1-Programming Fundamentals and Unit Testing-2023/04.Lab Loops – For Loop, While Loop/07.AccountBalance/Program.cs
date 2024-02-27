ced75ef6e587a01341d33149b5c95f1fb75da466
ani200208 <ani200208@proton.me>
2024-02-27 14:09:48 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/07.AccountBalance/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _07.AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalBalance = 0;
+
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                
+                if (input == "NoMoreMoney")
+                    break;
+
+                
+                double amount;
+                if (!double.TryParse(input, out amount))
+                {
+                    Console.WriteLine("Invalid operation!");
+                    return;
+                }
+
+                
+                if (amount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    return;
+                }
+
+                
+                totalBalance += amount;
+                Console.WriteLine("Increase: " + amount.ToString("F2"));
+            }
+
+            
+            Console.WriteLine("Total: " + totalBalance.ToString("F2"));
+        }
+    }
+    
+}
