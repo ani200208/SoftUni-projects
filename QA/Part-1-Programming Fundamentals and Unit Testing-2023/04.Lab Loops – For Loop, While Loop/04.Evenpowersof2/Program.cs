fad1562364954fb2b4652942c28fd3a1e3e7b298
ani200208 <ani200208@proton.me>
2024-02-27 14:00:59 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/04.Evenpowersof2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.Evenpowersof2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+            for (int power = 0; power <= n; power += 2)
+            {
+                Console.WriteLine(Math.Pow(2, power));
+            }
+        }
+    }
+}
