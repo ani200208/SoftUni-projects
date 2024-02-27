594d27d89a215da2c3b6b1392b2b3b1285aa67b0
ani200208 <ani200208@proton.me>
2024-02-27 14:03:04 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/04.Lab Loops – For Loop, While Loop/05.SumOfNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialNumber = int.Parse(Console.ReadLine());
+
+            
+            int sum = 0;
+            int currentNumber;
+
+            
+            while (sum < initialNumber)
+            {
+                currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            
+            Console.WriteLine(sum);
+        }
+    }
+    
+}
