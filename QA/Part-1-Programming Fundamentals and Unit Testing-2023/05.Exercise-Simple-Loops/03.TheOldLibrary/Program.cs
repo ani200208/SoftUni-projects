184e83eb865d0d8ed82af19bc99702916d87f26b
ani200208 <ani200208@proton.me>
2024-02-28 16:36:17 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/03.TheOldLibrary/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.TheOldLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteBook = Console.ReadLine();
+
+
+
+            int booksChecked = 0;
+            bool bookFound = false;
+
+            
+            while (true)
+            {
+                string book = Console.ReadLine();
+
+                
+                if (book == "No More Books")
+                {
+                    
+                    if (!bookFound)
+                        Console.WriteLine($"The book you search is not here!\nYou checked {booksChecked} books.");
+                    break; 
+                }
+
+                
+                booksChecked++;
+
+                
+                if (book == "Troy") 
+                {
+                    bookFound = true;
+                    Console.WriteLine($"You checked {booksChecked} books and found it.");
+                    break; 
+                }
+            }
+        }
+    }
+}
