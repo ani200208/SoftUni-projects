e0d922083dfc58f228d2dd60b4c911dd61b052d6
ani200208 <ani200208@proton.me>
2024-02-28 16:24:55 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/02.ElementEqualsSumOfOthers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.ElementEqualsSumOfOthers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+            int sum = 0;
+            int max = int.MinValue;
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            
+            int diff = Math.Abs(max - (sum - max));
+
+           
+            if (sum - max == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
