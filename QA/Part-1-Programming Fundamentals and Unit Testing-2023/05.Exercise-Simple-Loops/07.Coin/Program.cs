144eee1640c17a4052df383a4df40620e7254d32
ani200208 <ani200208@proton.me>
2024-02-28 16:46:39 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/07.Coin/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.Coin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double change = double.Parse(Console.ReadLine());
+
+            
+            int changeInStotinki = (int)(change * 100);
+
+            
+            int[] coinValues = { 200, 100, 50, 20, 10, 5, 2, 1 };
+
+            
+            int coinCount = 0;
+
+            
+            for (int i = 0; i < coinValues.Length; i++)
+            {
+                
+                int coinsNeeded = changeInStotinki / coinValues[i];
+
+                
+                coinCount += coinsNeeded;
+
+                
+                changeInStotinki %= coinValues[i];
+            }
+
+            Console.WriteLine(coinCount);
+        }
+    }
+    
+}
