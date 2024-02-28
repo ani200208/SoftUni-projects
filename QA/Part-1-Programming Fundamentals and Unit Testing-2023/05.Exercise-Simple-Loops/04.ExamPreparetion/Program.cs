ab0bf95d6dda2eba32d2a43ff504ab31c7f61c3c
ani200208 <ani200208@proton.me>
2024-02-28 16:38:56 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/04.ExamPreparetion/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04.ExamPreparetion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxPoorGrades = int.Parse(Console.ReadLine());
+
+            // Initialize variables
+            int poorGradesCount = 0;
+            int solvedProblemsCount = 0;
+            double totalGrade = 0;
+            string lastProblem = "";
+            bool isFailed = true;
+
+            // Main loop
+            while (poorGradesCount < maxPoorGrades)
+            {
+                string problemName = Console.ReadLine();
+                if (problemName == "Enough")
+                {
+                    isFailed = false;
+                    break;
+                }
+
+                int grade = int.Parse(Console.ReadLine());
+
+                // Update total grade and problem count
+                totalGrade += grade;
+                solvedProblemsCount++;
+                lastProblem = problemName;
+
+                // Check if the grade is poor
+                if (grade <= 4)
+                    poorGradesCount++;
+            }
+
+            // Print output
+            if (isFailed)
+            {
+                Console.WriteLine($"You need a break, {poorGradesCount} poor grades.");
+            }
+            else
+            {
+                double averageGrade = totalGrade / solvedProblemsCount;
+                Console.WriteLine($"Average score: {averageGrade:F2}");
+                Console.WriteLine($"Number of problems: {solvedProblemsCount}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
