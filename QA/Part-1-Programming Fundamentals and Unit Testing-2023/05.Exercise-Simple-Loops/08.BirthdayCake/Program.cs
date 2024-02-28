3218bdcb16569b616b5f842c44b3c0b4cdb17b18
ani200208 <ani200208@proton.me>
2024-02-28 16:48:35 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/08.BirthdayCake/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _08.BirthdayCake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int cakePieces = width * length;
+
+            string input;
+            while ((input = Console.ReadLine()) != "STOP")
+            {
+                int takenPieces = int.Parse(input);
+                cakePieces -= takenPieces;
+
+                if (cakePieces < 0)
+                {
+                    int missingPieces = Math.Abs(cakePieces);
+                    Console.WriteLine($"No more cake left! You need {missingPieces} pieces more.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{cakePieces} pieces are left.");
+        }
+    }
+    
+}
