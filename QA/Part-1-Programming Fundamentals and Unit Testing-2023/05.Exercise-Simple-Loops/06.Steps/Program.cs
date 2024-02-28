23ef579c1a7bc4b3d0871d053717c8f6bdb0732e
ani200208 <ani200208@proton.me>
2024-02-28 16:44:38 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/06.Steps/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _06.Steps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = 10000; 
+            int totalSteps = 0; 
+
+            string input;
+            while ((input = Console.ReadLine()) != "Going home")
+            {
+                int steps = int.Parse(input);
+                totalSteps += steps;
+
+                if (totalSteps >= goal)
+                {
+                    int difference = totalSteps - goal;
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{difference} steps over the goal!");
+                    return;
+                }
+            }
+
+            int stepsGoingHome = int.Parse(Console.ReadLine());
+            totalSteps += stepsGoingHome;
+
+            if (totalSteps >= goal)
+            {
+                int difference = totalSteps - goal;
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{difference} steps over the goal!");
+            }
+            else
+            {
+                int difference = goal - totalSteps;
+                Console.WriteLine($"{difference} more steps to reach goal.");
+            }
+        }
+    }
+}
