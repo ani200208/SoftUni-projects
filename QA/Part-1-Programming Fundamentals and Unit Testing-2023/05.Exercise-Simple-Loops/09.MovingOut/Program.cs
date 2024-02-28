96c0ab4c58f01e363752b2ee3d2fc8baa2dc1841
ani200208 <ani200208@proton.me>
2024-02-28 16:50:17 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.Exercise-Simple-Loops/09.MovingOut/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _09.MovingOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int totalVolume = width * length * height;
+
+            string input;
+            while ((input = Console.ReadLine()) != "Done")
+            {
+                int boxesVolume = int.Parse(input);
+                totalVolume -= boxesVolume;
+
+                if (totalVolume <= 0)
+                {
+                    int neededVolume = Math.Abs(totalVolume);
+                    Console.WriteLine($"No more free space! You need {neededVolume} Cubic meters more.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{totalVolume} Cubic meters left.");
+        }
+    }
+    
+}
