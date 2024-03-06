c2d44c76cf57a5880bd00cdfed9a70e78fae2d65
ani200208 <ani200208@proton.me>
2024-03-06 17:02:51 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/05.RepeatString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Enter repeat count:");
+            int repeatCount = Convert.ToInt32(Console.ReadLine());
+
+            string repeatedString = RepeatString(text, repeatCount);
+            Console.WriteLine("Result: " + repeatedString);
+        }
+
+        static string RepeatString(string text, int repeatCount)
+        {
+            string result = "";
+
+            for (int i = 0; i < repeatCount; i++)
+            {
+                result += text;
+            }
+
+            return result;
+        }
+    }
+}
+
