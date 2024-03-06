4b8634de6b10126c9545ca696a0872fd9868df39
ani200208 <ani200208@proton.me>
2024-03-06 16:52:09 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/01.SignOfIntegerNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.SignOfIntegerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            PrintSign(number);
+        }
+
+        static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
+
