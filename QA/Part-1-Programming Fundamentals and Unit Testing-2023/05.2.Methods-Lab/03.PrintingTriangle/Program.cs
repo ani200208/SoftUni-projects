fcbf4a332f61c757ec99360f4d0ae84cb49de796
ani200208 <ani200208@proton.me>
2024-03-06 16:58:43 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/03.PrintingTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        static void PrintTriangle(int n)
+        {
+            
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
