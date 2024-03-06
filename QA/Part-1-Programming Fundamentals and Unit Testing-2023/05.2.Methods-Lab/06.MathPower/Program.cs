5a11bca9b56c78f23ebc4dbe4a1bf2428a98e847
ani200208 <ani200208@proton.me>
2024-03-06 17:04:25 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/06.MathPower/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the base number:");
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the power:");
+            int power = Convert.ToInt32(Console.ReadLine());
+
+            int result = CalculatePower(baseNumber, power);
+            Console.WriteLine("Result: " + result);
+        }
+
+        static int CalculatePower(int baseNumber, int power)
+        {
+            int result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+    }
+}
+
