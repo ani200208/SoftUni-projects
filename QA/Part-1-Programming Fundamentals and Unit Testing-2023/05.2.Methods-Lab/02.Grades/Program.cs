ffcdd1c69ac8c3a622561a6fb30113297b0b1265
ani200208 <ani200208@proton.me>
2024-03-06 16:56:25 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/02.Grades/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a grade:");
+            double grade = Convert.ToDouble(Console.ReadLine());
+            PrintGradeInWords(grade);
+        }
+
+        static void PrintGradeInWords(double grade)
+        {
+            if (grade >= 2.00 && grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                Console.WriteLine("Average");
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade entered. Grade must be between 2.00 and 6.00.");
+            }
+        }
+    }
+}
+
