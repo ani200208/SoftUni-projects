ace29a1afb1a189d96e83d13db869125721c63a9
ani200208 <ani200208@proton.me>
2024-03-06 17:06:25 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/07. GreaterofTwoValues/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _07._GreaterofTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the type (int, char, or string):");
+            string type = Console.ReadLine();
+
+            Console.WriteLine("Enter the first value:");
+            string value1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the second value:");
+            string value2 = Console.ReadLine();
+
+            string result = "";
+
+            if (type == "int")
+            {
+                result = CompareInt(int.Parse(value1), int.Parse(value2)).ToString();
+            }
+            else if (type == "char")
+            {
+                result = CompareChar(char.Parse(value1), char.Parse(value2)).ToString();
+            }
+            else if (type == "string")
+            {
+                result = CompareString(value1, value2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid type entered.");
+                return;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+
+        static int CompareInt(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+
+        static char CompareChar(char a, char b)
+        {
+            return a > b ? a : b;
+        }
+
+        static string CompareString(string a, string b)
+        {
+            return string.Compare(a, b) > 0 ? a : b;
+        }
+    }
+}
+
