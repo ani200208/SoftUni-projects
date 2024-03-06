b18c39a2753638080b698e2a3cb38e84328a1ce7
ani200208 <ani200208@proton.me>
2024-03-06 17:00:36 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.2.Methods-Lab/04.CalculateRectangleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int area = CalculateRectangleArea(width, length);
+            Console.WriteLine("The area of the rectangle is: " + area);
+        }
+
+        static int CalculateRectangleArea(int width, int length)
+        {
+            return width * length;
+        }
+    }
+}
+
