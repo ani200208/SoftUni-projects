18f336f9371f00164753d844aecf845d2c3aca5e
ani200208 <ani200208@proton.me>
2024-03-08 15:48:24 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.3. Nested-Loops-And-Methods-Exercise/04.LetterCombinations/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.LetterCombinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            char excluded = char.Parse(Console.ReadLine());
+
+            GenerateCombinations(start, end, excluded);
+        }
+
+        static void GenerateCombinations(char start, char end, char excluded)
+        {
+            int count = 0;
+            for (char i = start; i <= end; i++)
+            {
+                if (i != excluded)
+                {
+                    for (char j = start; j <= end; j++)
+                    {
+                        if (j != excluded)
+                        {
+                            for (char k = start; k <= end; k++)
+                            {
+                                if (k != excluded)
+                                {
+                                    Console.Write($"{i}{j}{k} ");
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(count);
+        }
+    }
+}
+
