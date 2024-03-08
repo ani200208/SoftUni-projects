f082f31bee0a6a1cfc509542ac60b3125baaafb3
ani200208 <ani200208@proton.me>
2024-03-08 15:38:25 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.3. Nested-Loops-And-Methods-Exercise/01.NestedLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.NestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= N; i += 2)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        int product = i * j;
+                        Console.Write($"{i}{j}{product} ");
+                    }
+                }
+            }
+        }
+    }
+}
