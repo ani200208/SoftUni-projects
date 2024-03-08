c64cd90f3b869857c824b38b6697832dcdfc3e3b
ani200208 <ani200208@proton.me>
2024-03-08 15:51:57 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.3. Nested-Loops-And-Methods-Exercise/06.VowelsCount/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int vowelsCount = CountVowels(text);
+
+            Console.WriteLine(vowelsCount);
+        }
+
+        static int CountVowels(string text)
+        {
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (IsVowel(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static bool IsVowel(char c)
+        {
+            c = char.ToLower(c);
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+    }
+}
