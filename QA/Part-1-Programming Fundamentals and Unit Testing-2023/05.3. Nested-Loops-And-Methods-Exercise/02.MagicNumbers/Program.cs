e5d133d3a3c42bf75671a0dfb1d7cec44376ba14
ani200208 <ani200208@proton.me>
2024-03-08 15:40:51 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.3. Nested-Loops-And-Methods-Exercise/02.MagicNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.MagicNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 100; i <= 999; i++)
+            {
+                int digit1 = i / 100;    
+                int digit2 = (i / 10) % 10;  
+                int digit3 = i % 10;    
+
+                if (digit1 * digit2 * digit3 == N)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
