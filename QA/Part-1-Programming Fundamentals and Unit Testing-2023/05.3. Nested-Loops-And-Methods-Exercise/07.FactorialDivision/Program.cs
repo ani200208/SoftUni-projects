20cade139fcdfe2a27dc439a839cdbc4b1c996f2
ani200208 <ani200208@proton.me>
2024-03-08 15:53:23 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.3. Nested-Loops-And-Methods-Exercise/07.FactorialDivision/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _07.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            long factorial1 = CalculateFactorial(number1);
+            long factorial2 = CalculateFactorial(number2);
+
+            long result = factorial1 / factorial2;
+
+            Console.WriteLine(result);
+        }
+
+        static long CalculateFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
