3b97c359385346fa6ca9784e84145e76a5c0cddf
ani200208 <ani200208@proton.me>
2024-03-08 15:55:21 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/05.3. Nested-Loops-And-Methods-Exercise/08.MultiplicationSign/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _08.MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            string result = GetProductSign(num1, num2, num3);
+
+            Console.WriteLine(result);
+        }
+
+        static string GetProductSign(int num1, int num2, int num3)
+        {
+            int negativesCount = 0;
+
+            if (num1 < 0)
+                negativesCount++;
+            if (num2 < 0)
+                negativesCount++;
+            if (num3 < 0)
+                negativesCount++;
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+                return "zero";
+            else if (negativesCount % 2 == 0)
+                return "positive";
+            else
+                return "negative";
+        }
+    }
+}
