2d0b6fa0f972c3f735a70b411ec85051805aa52b
ani200208 <ani200208@proton.me>
2024-03-09 10:35:52 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.Exercise-Nested-Loops/05.SpecialNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace _05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 1111; num <= 9999; num++)
+            {
+                int currentNumber = num;
+                bool isSpecial = true;
+
+                while (currentNumber > 0)
+                {
+                    int digit = currentNumber % 10;
+                    currentNumber /= 10;
+
+                    if (digit == 0 || n % digit != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
