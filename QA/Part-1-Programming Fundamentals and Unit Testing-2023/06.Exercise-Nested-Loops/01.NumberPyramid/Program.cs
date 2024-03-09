6324fe75afb67952d328cf4b98eabb8f4bfb37b9
ani200208 <ani200208@proton.me>
2024-03-09 10:24:15 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.Exercise-Nested-Loops/01.NumberPyramid/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _01.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a whole number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int num = 1;
+            bool exitOuterLoop = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool exitInnerLoop = false;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (num > n)
+                    {
+                        exitInnerLoop = true;
+                        break;
+                    }
+
+                    Console.Write(num + " ");
+                    num++;
+                }
+
+                if (exitInnerLoop)
+                {
+                    exitOuterLoop = true;
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+
+            if (!exitOuterLoop)
+            {
+                Console.WriteLine();
+                for (int i = n + 1; ; i += 4)
+                {
+                    for (int j = 1; j <= 4; j++)
+                    {
+                        if (num > n)
+                        {
+                            exitOuterLoop = true;
+                            break;
+                        }
+
+                        Console.Write(num + " ");
+                        num++;
+                    }
+
+                    Console.WriteLine();
+
+                    if (exitOuterLoop)
+                        break;
+                }
+            }
+        }
+}    }
+
---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.Exercise-Nested-Loops/02.EqualSumsOfEvenAndOddPositions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02.EqualSumsOfEvenAndOddPositions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
