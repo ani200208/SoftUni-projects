f91cc65185f171d092813d43262b01f1ba333fae
ani200208 <ani200208@proton.me>
2024-03-09 10:26:44 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.Exercise-Nested-Loops/02.EqualSumsOfEvenAndOddPositions/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.Exercise-Nested-Loops/02.EqualSumsOfEvenAndOddPositions/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the first six-digit number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second six-digit number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                string numberString = i.ToString();
+                int evenSum = 0;
+                int oddSum = 0;
+
+                for (int j = 0; j < numberString.Length; j++)
+                {
+                    int digit = int.Parse(numberString[j].ToString());
+                    if ((j + 1) % 2 == 0)
+                        evenSum += digit;
+                    else
+                        oddSum += digit;
+                }
+
+                if (evenSum == oddSum)
+                    Console.Write(i + " ");
+            }
+
+            Console.WriteLine(); 
         }
     }
 }
