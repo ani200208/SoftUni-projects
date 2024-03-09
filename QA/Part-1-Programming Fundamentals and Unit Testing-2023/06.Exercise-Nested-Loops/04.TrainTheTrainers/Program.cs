1adf1320248676abac8d2e97af46c463a3bc2f44
ani200208 <ani200208@proton.me>
2024-03-09 10:34:00 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.Exercise-Nested-Loops/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of jury members: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double totalSum = 0;
+            int presentationCount = 0;
+
+            while (true)
+            {
+                string presentation = Console.ReadLine();
+
+                if (presentation == "Finish")
+                    break;
+
+                double presentationSum = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    presentationSum += grade;
+                }
+
+                double averageGrade = presentationSum / n;
+                totalSum += averageGrade;
+                presentationCount++;
+
+                Console.WriteLine($"{presentation} - {averageGrade:f2}.");
+            }
+
+            double overallAverage = totalSum / presentationCount;
+            Console.WriteLine($"Student's final assessment is {overallAverage:f2}.");
+        }
+    }
+}
+
