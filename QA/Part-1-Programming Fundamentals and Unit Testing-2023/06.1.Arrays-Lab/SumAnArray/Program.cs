403e8e0f960f645de5bf044dabab41b666322a52
ani200208 <ani200208@proton.me>
2024-03-12 13:05:45 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/SumAnArray/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/SumAnArray/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            Console.WriteLine("Hello, World!");
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter integers separated by space:");
+                string input = Console.ReadLine();
+
+                string[] numbersAsString = input.Split(' ');
+                int[] numbers = new int[numbersAsString.Length];
+
+                for (int i = 0; i < numbersAsString.Length; i++)
+                {
+                    numbers[i] = int.Parse(numbersAsString[i]);
+                }
+
+                int sum = 0;
+                foreach (int number in numbers)
+                {
+                    sum += number;
+                }
+
+                Console.WriteLine("Sum: " + sum);
+            }
         }
     }
 }
