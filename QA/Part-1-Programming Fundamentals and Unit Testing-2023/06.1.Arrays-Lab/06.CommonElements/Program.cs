38dcad01c7107bb7598dd79b6cd02da24ed330cf
ani200208 <ani200208@proton.me>
2024-03-12 13:16:28 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/06.CommonElements/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/06.CommonElements/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            Console.WriteLine("Hello, World!");
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter elements of the first array separated by space:");
+                string input1 = Console.ReadLine();
+                Console.WriteLine("Enter elements of the second array separated by space:");
+                string input2 = Console.ReadLine();
+
+                string[] array1 = input1.Split(' ');
+                string[] array2 = input2.Split(' ');
+
+                if (array1.Length != array2.Length)
+                {
+                    Console.WriteLine("Arrays must have the same length.");
+                    return;
+                }
+
+                HashSet<string> set = new HashSet<string>();
+
+                foreach (string element in array1)
+                {
+                    set.Add(element);
+                }
+
+                Console.Write("Common elements: ");
+                foreach (string element in array2)
+                {
+                    if (set.Contains(element))
+                    {
+                        Console.Write(element + " ");
+                    }
+                }
+            }
         }
     }
 }
