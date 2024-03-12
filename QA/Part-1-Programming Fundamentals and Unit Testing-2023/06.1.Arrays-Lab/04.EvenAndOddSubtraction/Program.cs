22615b2d4b27064b74a3d3ee3b281a10a2cdecf2
ani200208 <ani200208@proton.me>
2024-03-12 13:12:04 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/04.EvenAndOddSubtraction/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/04.EvenAndOddSubtraction/Program.cs
@@ -2,9 +2,48 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            Console.WriteLine("Hello, World!");
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter integers separated by space:");
+                string input = Console.ReadLine();
+
+                string[] numbersAsString = input.Split(' ');
+                int[] numbers = new int[numbersAsString.Length];
+
+                for (int i = 0; i < numbersAsString.Length; i++)
+                {
+                    if (int.TryParse(numbersAsString[i], out int num))
+                    {
+                        numbers[i] = num;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input: " + numbersAsString[i]);
+                        return; 
+                    }
+                }
+
+                int sumEven = 0;
+                int sumOdd = 0;
+
+                foreach (int number in numbers)
+                {
+                    if (number % 2 == 0)
+                    {
+                        sumEven += number;
+                    }
+                    else
+                    {
+                        sumOdd += number;
+                    }
+                }
+
+                int difference = sumEven - sumOdd;
+
+                Console.WriteLine("Difference: " + difference);
+            }
         }
     }
 }
