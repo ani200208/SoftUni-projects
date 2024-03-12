e90f084c09069fcb42d62c3cee16dca5e461145c
ani200208 <ani200208@proton.me>
2024-03-12 13:14:58 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/05.EqualArrays/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/05.EqualArrays/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            Console.WriteLine("Hello, World!");
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter elements of the first array separated by space:");
+                string input1 = Console.ReadLine();
+                Console.WriteLine("Enter elements of the second array separated by space:");
+                string input2 = Console.ReadLine();
+
+                string[] array1 = input1.Split(' ');
+                string[] array2 = input2.Split(' ');
+
+                if (array1.Length != array2.Length)
+                {
+                    Console.WriteLine("Arrays are not identical.");
+                    return;
+                }
+
+                bool identical = true;
+
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    if (array1[i] != array2[i])
+                    {
+                        identical = false;
+                        break;
+                    }
+                }
+
+                if (identical)
+                {
+                    Console.WriteLine("Arrays are identical.");
+                }
+                else
+                {
+                    Console.WriteLine("Arrays are not identical.");
+                }
+            }
         }
     }
 }
