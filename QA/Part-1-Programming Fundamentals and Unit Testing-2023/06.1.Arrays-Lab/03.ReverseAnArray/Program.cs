a4dcc1ffeac74c9a95b914b8f47512adbd25b245
ani200208 <ani200208@proton.me>
2024-03-12 13:08:12 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/03.ReverseAnArray/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/03.ReverseAnArray/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            Console.WriteLine("Hello, World!");
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the number of elements:");
+                int N = int.Parse(Console.ReadLine());
+
+                int[] numbers = new int[N];
+
+                Console.WriteLine("Enter the numbers:");
+
+                
+                for (int i = 0; i < N; i++)
+                {
+                    numbers[i] = int.Parse(Console.ReadLine());
+                }
+
+                
+                for (int i = 0; i < N / 2; i++)
+                {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[N - i - 1];
+                    numbers[N - i - 1] = temp;
+                }
+
+                
+                Console.Write("Reversed array: ");
+                foreach (int number in numbers)
+                {
+                    Console.Write(number + " ");
+                }
+            }
         }
     }
 }
---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.1.Arrays-Lab/04.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04.EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
