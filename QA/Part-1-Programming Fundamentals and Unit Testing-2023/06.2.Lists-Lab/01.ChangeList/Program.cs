bc2225a188d2297889e65f28b14f2dfcf012db7b
ani200208 <ani200208@proton.me>
2024-03-13 15:13:22 +0200

    Updating my repo

--- QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.2.Lists-Lab/01.ChangeList/Program.cs
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.2.Lists-Lab/01.ChangeList/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                if (action == "Delete")
+                {
+                    int element = int.Parse(tokens[1]);
+                    numbers.RemoveAll(x => x == element);
+                }
+                else if (action == "Insert")
+                {
+                    int element = int.Parse(tokens[1]);
+                    int position = int.Parse(tokens[2]);
+                    numbers.Insert(position, element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
