6d326b84f212ee90efce73b0d71dda120ce509c8
ani200208 <ani200208@proton.me>
2024-03-13 15:16:52 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.2.Lists-Lab/03.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> positiveNumbers = numbers.Where(x => x >= 0).ToList();
+
+            if (positiveNumbers.Count > 0)
+            {
+                positiveNumbers.Reverse();
+                Console.WriteLine(string.Join(" ", positiveNumbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
