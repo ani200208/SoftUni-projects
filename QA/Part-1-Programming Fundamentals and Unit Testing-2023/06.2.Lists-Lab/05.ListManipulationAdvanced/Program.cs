ed93d30e9f3ee8c3fef069ba54e31862b7c605d1
ani200208 <ani200208@proton.me>
2024-03-13 15:21:03 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.2.Lists-Lab/05.ListManipulationAdvanced/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _05.ListManipulationAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                if (action == "Contains")
+                {
+                    int number = int.Parse(tokens[1]);
+                    if (numbers.Contains(number))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such number");
+                    }
+                }
+                else if (action == "PrintEven")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 == 0)));
+                }
+                else if (action == "PrintOdd")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+                }
+                else if (action == "GetSum")
+                {
+                    Console.WriteLine(numbers.Sum());
+                }
+                else if (action == "Filter")
+                {
+                    string condition = tokens[1];
+                    int number = int.Parse(tokens[2]);
+
+                    if (condition == ">")
+                    {
+                        numbers = numbers.Where(x => x > number).ToList();
+                    }
+                    else if (condition == "<")
+                    {
+                        numbers = numbers.Where(x => x < number).ToList();
+                    }
+                    else if (condition == ">=")
+                    {
+                        numbers = numbers.Where(x => x >= number).ToList();
+                    }
+                    else if (condition == "<=")
+                    {
+                        numbers = numbers.Where(x => x <= number).ToList();
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
