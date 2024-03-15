be911d05591750e96d9342fd54adf6a843419b57
ani200208 <ani200208@proton.me>
2024-03-15 15:20:12 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/03.MergingLists/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first sequence of integers separated by spaces:");
+            List<int> firstList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            Console.WriteLine("Enter the second sequence of integers separated by spaces:");
+            List<int> secondList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> mergedList = MergeLists(firstList, secondList);
+
+            Console.WriteLine("Merged list:");
+            Console.WriteLine(string.Join(" ", mergedList));
+        }
+
+        static List<int> MergeLists(List<int> firstList, List<int> secondList)
+        {
+            List<int> mergedList = new List<int>();
+
+            int minLength = Math.Min(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                mergedList.Add(firstList[i]);
+                mergedList.Add(secondList[i]);
+            }
+
+         
+            if (firstList.Count > secondList.Count)
+                mergedList.AddRange(firstList.Skip(minLength));
+            else if (secondList.Count > firstList.Count)
+                mergedList.AddRange(secondList.Skip(minLength));
+
+            return mergedList;
+        }
+    }
+}
+
