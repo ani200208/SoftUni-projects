775bdb03c3945ee7f1f0b2062e67b6ddf3badbe4
ani200208 <ani200208@proton.me>
2024-03-15 15:33:25 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/07.Zig-ZagArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int num1 = int.Parse(input[0]);
+                int num2 = int.Parse(input[1]);
+
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = num1;
+                    secondArray[i] = num2;
+                }
+                else
+                {
+                    firstArray[i] = num2;
+                    secondArray[i] = num1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+        }
+    }
+}
