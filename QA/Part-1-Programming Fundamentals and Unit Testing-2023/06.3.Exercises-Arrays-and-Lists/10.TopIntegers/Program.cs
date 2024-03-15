d9137d077eaaae9a945f188760c42f40e2c50786
ani200208 <ani200208@proton.me>
2024-03-15 15:39:58 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/10.TopIntegers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _10.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            
+            List<int> topIntegers = FindTopIntegers(numbers);
+
+            
+            Console.WriteLine(string.Join(" ", topIntegers));
+        }
+
+        static List<int> FindTopIntegers(int[] numbers)
+        {
+            List<int> topIntegers = new List<int>();
+
+           
+            int maxRight = numbers[numbers.Length - 1];
+            topIntegers.Add(maxRight);
+
+            for (int i = numbers.Length - 2; i >= 0; i--)
+            {
+                if (numbers[i] > maxRight)
+                {
+                    topIntegers.Add(numbers[i]);
+                    maxRight = numbers[i];
+                }
+            }
+
+           
+            topIntegers.Reverse();
+
+            return topIntegers;
+        }
+    }
+}
