e6baf25c87fa91e7b23adf0e26f5f18979699b2c
ani200208 <ani200208@proton.me>
2024-03-15 15:38:16 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/09.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _09.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+           
+            int[] longestSequence = FindLongestSequence(numbers);
+
+           
+            Console.WriteLine(string.Join(" ", longestSequence));
+        }
+
+        static int[] FindLongestSequence(int[] numbers)
+        {
+            int maxLength = 0;
+            int maxStartIndex = 0;
+
+            int currentLength = 1;
+            int currentStartIndex = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentLength++;
+
+                   
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        maxStartIndex = currentStartIndex;
+                    }
+                }
+                else
+                {
+                    currentLength = 1;
+                    currentStartIndex = i;
+                }
+            }
+
+            
+            int[] longestSequence = new int[maxLength];
+            Array.Copy(numbers, maxStartIndex, longestSequence, 0, maxLength);
+
+            return longestSequence;
+        }
+    }
+}
