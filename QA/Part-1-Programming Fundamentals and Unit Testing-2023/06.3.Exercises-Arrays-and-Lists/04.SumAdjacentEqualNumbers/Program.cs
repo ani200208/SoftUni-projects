00998ed5558f498cd73806328d906f789e08ed06
ani200208 <ani200208@proton.me>
2024-03-15 15:27:52 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/04.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _04.SumAdjacentEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sequence of integer numbers separated by spaces:");
+            string input = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+
+            foreach (string number in input.Split(' '))
+            {
+                numbers.Add(int.Parse(number));
+            }
+
+            List<int> summedList = SumAdjacentEqualNumbers(numbers);
+
+            Console.WriteLine("Summed list:");
+            Console.WriteLine(string.Join(" ", summedList));
+        }
+
+        static List<int> SumAdjacentEqualNumbers(List<int> numbers)
+        {
+            List<int> summedList = new List<int>();
+
+            int i = 0;
+            while (i < numbers.Count)
+            {
+                int currentNumber = numbers[i];
+                int sum = currentNumber;
+
+                // Find adjacent equal numbers and sum them
+                while (i < numbers.Count - 1 && numbers[i] == numbers[i + 1])
+                {
+                    sum += numbers[i + 1];
+                    i++;
+                }
+
+                summedList.Add(sum);
+                i++;
+            }
+
+            return summedList;
+        }
+    }
+}
---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/05.BombNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sequence of integer numbers separated by spaces:");
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            Console.WriteLine("Enter the special bomb number followed by its power separated by space:");
+            int[] bombParams = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int bombNumber = bombParams[0];
+            int bombPower = bombParams[1];
+
+            List<int> remainingNumbers = DetonateBomb(numbers, bombNumber, bombPower);
+
+            int sum = remainingNumbers.Sum();
+
+            Console.WriteLine("Sum of remaining numbers: " + sum);
+        }
+
+        static List<int> DetonateBomb(List<int> numbers, int bombNumber, int bombPower)
+        {
+            while (numbers.Contains(bombNumber))
+            {
+                int index = numbers.IndexOf(bombNumber);
+
+                int leftIndex = Math.Max(0, index - bombPower);
+                int rightIndex = Math.Min(numbers.Count - 1, index + bombPower);
+
+                numbers.RemoveRange(leftIndex, rightIndex - leftIndex + 1);
+            }
+
+            return numbers;
+        }
+    }
+}
