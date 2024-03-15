240dcffe7c0fbdecd7471bd613a603efacd35e8b
ani200208 <ani200208@proton.me>
2024-03-15 15:18:06 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/02.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.CondenseArrayToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sequence of integers separated by spaces:");
+            string input = Console.ReadLine();
+
+            int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+            int result = CondenseArray(numbers);
+
+            Console.WriteLine("Condensed result: " + result);
+        }
+
+        static int CondenseArray(int[] numbers)
+        {
+            while (numbers.Length > 1)
+            {
+                int[] condensedArray = new int[numbers.Length - 1];
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    condensedArray[i] = numbers[i] + numbers[i + 1];
+                }
+
+                numbers = condensedArray;
+            }
+
+            return numbers[0];
+        }
+    }
+}
