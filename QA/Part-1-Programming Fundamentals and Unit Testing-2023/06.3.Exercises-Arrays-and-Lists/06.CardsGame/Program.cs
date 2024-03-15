3d6db1f2aa8c7d70c70aa11ef1666795e50fb5d3
ani200208 <ani200208@proton.me>
2024-03-15 15:30:04 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/06.CardsGame/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _06.CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayerCards = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayerCards = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (firstPlayerCards.Count > 0 && secondPlayerCards.Count > 0)
+            {
+                int firstPlayerCard = firstPlayerCards[0];
+                int secondPlayerCard = secondPlayerCards[0];
+
+                if (firstPlayerCard > secondPlayerCard)
+                {
+                    firstPlayerCards.RemoveAt(0);
+                    secondPlayerCards.RemoveAt(0);
+                    firstPlayerCards.Add(firstPlayerCard);
+                    firstPlayerCards.Add(secondPlayerCard);
+                }
+                else if (secondPlayerCard > firstPlayerCard)
+                {
+                    firstPlayerCards.RemoveAt(0);
+                    secondPlayerCards.RemoveAt(0);
+                    secondPlayerCards.Add(secondPlayerCard);
+                    secondPlayerCards.Add(firstPlayerCard);
+                }
+                else // if both cards are equal
+                {
+                    firstPlayerCards.RemoveAt(0);
+                    secondPlayerCards.RemoveAt(0);
+                }
+            }
+
+            if (firstPlayerCards.Count > 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstPlayerCards.Sum()}");
+            }
+            else if (secondPlayerCards.Count > 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondPlayerCards.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine("Draw!");
+            }
+        }
+    }
+}
