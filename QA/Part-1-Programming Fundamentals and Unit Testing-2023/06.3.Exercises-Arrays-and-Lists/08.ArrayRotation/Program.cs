2d7ad47e43549a39c1d5874437780053563ba661
ani200208 <ani200208@proton.me>
2024-03-15 15:35:51 +0200

    Updating my repo

---
+++ QA/Part-1-Programming Fundamentals and Unit Testing-2023/06.3.Exercises-Arrays-and-Lists/08.ArrayRotation/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _08.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] numbersStr = Console.ReadLine().Split();
+            int[] numbers = new int[numbersStr.Length];
+
+            for (int i = 0; i < numbersStr.Length; i++)
+            {
+                numbers[i] = int.Parse(numbersStr[i]);
+            }
+
+           
+            int rotations = int.Parse(Console.ReadLine());
+
+           
+            RotateArray(numbers, rotations);
+
+            
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static void RotateArray(int[] arr, int rotations)
+        {
+            int length = arr.Length;
+
+            
+            for (int i = 0; i < rotations; i++)
+            {
+                int firstElement = arr[0];
+                for (int j = 0; j < length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[length - 1] = firstElement;
+            }
+        }
+    }
+}
